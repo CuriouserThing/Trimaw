cbcc58ebe102c89fd5d58db613b6dd3ec5cda763
curiouserthing <adelaidekwalter@gmail.com>
2026-09-25 21:09:33 -0400

    Use the scene that makes the Shredder smaller

--- Core/Models/Monsters/Figments/TiacauhShredderFigment.cs
+++ Core/Models/Monsters/Figments/TiacauhShredderFigment.cs
@@ -2,6 +2,7 @@ using Trimaw.Core.Animation;
 using Trimaw.Core.ImaginationSystem.UniqueIntents;
 using Trimaw.Core.Models.Powers;
 using Trimaw.Core.Models.Powers.Triggers;
+using Trimaw.Core.Utils;
 
 namespace Trimaw.Core.Models.Monsters.Figments;
 
@@ -9,6 +10,8 @@ public class TiacauhShredderFigment : SimpleFigment<MahuizzotiaTrigger, Placehol
 {
     protected override AkCombatSkeleton Skeleton => AkCombatSkeleton.Build("enemy_1097_cclmbjk",
         new ActionAnimation("Attack", 0.40, 1.40, 1.95, "Idle") { Timescale = 2.0 });
+
+    public override string CustomVisualPath => Pathfinder.Scene("tiacauh_shredder");
 
     protected override int InitialHp => 8;
     protected override int MaxHp => 12;
